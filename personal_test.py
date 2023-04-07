a9c24b8095c9f33696487575eb9021444a9ef0a0
tom-stephen <tom.stephen@ucalgary.ca>
2023-04-07 12:27:24 -0600

    trying to make the functions work

--- personal_test.py
+++ personal_test.py
@@ -2,7 +2,6 @@ from mylib.datastructures.linear.SLL import SinglyLinkedList as SLL
 from mylib.datastructures.nodes.Single_linked_Node import Node as SNode
 
 def test_SLL():
-    # test the circulat doubly linked list
     sll = SLL()
 
     # insert elements into the list

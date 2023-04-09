586b998e541e0d56c074cfffda9b2037cc5ee539
kevinUC17 <113135116+kevinUC17@users.noreply.github.com>
2023-04-09 15:21:18 -0600

    new AVL(work in progress)
    just uses BST and has no unique features

--- mylib/datastructures/trees/AVL.py
+++ mylib/datastructures/trees/AVL.py
@@ -1,69 +1,44 @@
+from mylib.datastructures.trees.TNode import TNode
+#from TNode import TNode
 
-class AVLTree:
-    from nodes.Tree_Node import Node
-    def __init__(self):
+from BST import BST
+
+class AVL(BST):
+
+
+    def __init__(self, val = 0, obj = None):
         self.root = None
+        if(val ==0 and obj == None):
+            super().__init__()
+        elif(obj == None):
+            super().__init__(val)
+        elif(val ==0):
+            super().__init__(obj)
+        
+    def insert(self, val):
+        super().insert(val)
+        print("root balance: ", super().getRoot().getBalance())
+        
+        
 
-    def _height(self, node):
-        if node is None:
-            return 0
-        return node.height
-
-    def _balance_factor(self, node):
-        if node is None:
-            return 0
-        return self._height(node.left) - self._height(node.right)
-
-    def _right_rotate(self, node):
-        new_root = node.left
-        node.left = new_root.right
-        new_root.right = node
-        node.height = 1 + max(self._height(node.left), self._height(node.right))
-        new_root.height = 1 + max(self._height(new_root.left), self._height(new_root.right))
-        return new_root
-
-    def _left_rotate(self, node):
-        new_root = node.right
-        node.right = new_root.left
-        new_root.left = node
-        node.height = 1 + max(self._height(node.left), self._height(node.right))
-        new_root.height = 1 + max(self._height(new_root.left), self._height(new_root.right))
-        return new_root
-
-    def _insert(self, node, key):
-        if node is None:
-            return self.Node(key)
-        if key < node.key:
-            node.left = self._insert(node.left, key)
-        else:
-            node.right = self._insert(node.right, key)
-        node.height = 1 + max(self._height(node.left), self._height(node.right))
-        balance = self._balance_factor(node)
-        if balance > 1 and key < node.left.key:
-            return self._right_rotate(node)
-        if balance < -1 and key > node.right.key:
-            return self._left_rotate(node)
-        if balance > 1 and key > node.left.key:
-            node.left = self._left_rotate(node.left)
-            return self._right_rotate(node)
-        if balance < -1 and key < node.right.key:
-            node.right = self._right_rotate(node.right)
-            return self._left_rotate(node)
-        return node
-
-    def insert(self, key):
-        self.root = self._insert(self.root, key)
-
-    def _inorder_traversal(self, node, result):
-        if node:
-            self._inorder_traversal(node.left, result)
-            result.append(node.key)
-            self._inorder_traversal(node.right, result)
-
-    def inorder_traversal(self):
-        result = []
-        self._inorder_traversal(self.root, result)
-        return result
+    def insertNode(self, node):
+        super().insert(node)
 
 
-          
+    def Search(self):
+        super().Search()
+
+    def printInOder(self):
+        super().printInOrder
+    def printBF(self):
+        super().printBF()
+
+if __name__ == "__main__":
+    print()
+    a = AVL(60)
+    a.insert(70)
+    a.insert(40)
+    a.insert(30)
+    a.insert(8)
+    a.printInOrder()
+    a.printBF()
